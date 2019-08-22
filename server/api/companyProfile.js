cc285875ec539f58da99fe5da573fbd34a40b85d
philezemonye <46997875+philezemonye@users.noreply.github.com>
2019-08-22 10:05:03 +0100

    Create company-profile  API. (#13)

---
+++ server/api/companyProfile.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const companyProfileDb = require("../services/database/companyProfile");
+
+/**
+ * The route here will be: /company-profile/ (remember the prefix company-profile is defined in api/index.js)
+ */
+router.get("/:id", (req, res) => {
+	const { id } = req.params;
+	companyProfileDb
+		.getCompanyProfile(id)
+		.then((data) => {
+			res.send(data);
+		})
+		.catch((err) => {
+			console.error(err);
+			res.send(500);
+		});
+});
+
+module.exports = router;
--- server/api/index.js
+++ server/api/index.js
@@ -5,12 +5,14 @@ const users = require("./users");
 const status = require("./status");
 const cities = require("./cities");
 const skills = require("./skills");
+const companyProfile = require("./companyProfile");
 const newOpportunity = require("./opportunities");
 
 router.use("/users", users);
 router.use("/status", status);
 router.use("/cities", cities);
 router.use("/skills", skills);
+router.use("/company-profile", companyProfile);
 router.use("/opportunities", newOpportunity);
 
 module.exports = router;
