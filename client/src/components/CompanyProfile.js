2ff0f80a01ecf7fd830c30a1fcd634d35834dad6
Phil Ezemonye <philezemonye@gmail.com>
2019-08-27 12:41:50 +0100

    client/src/components/CompanyProfile.js

--- client/src/components/CompanyProfile.js
+++ client/src/components/CompanyProfile.js
@@ -1,17 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Link } from "react-router-dom";
 import {
   Container,
-  Grid,
-  List,
-  Column,
   Button,
   Header,
-  Image,
-  Segment,
-  Divider
+  Divider,
+  Menu,
+  Dropdown,
+ 
 } from "semantic-ui-react";
 import { getCompanyProfile } from "../api/companyProfile";
+const options = [
+  { key: 1, text: <Menu.Item as={Link} to="/company/manage-profile">
+  Edit Profile
+</Menu.Item> , value: 1 },
+  { key: 2, text: <Menu.Item as={Link} to="/company/manage-profile">
+Delete Profile
+</Menu.Item>, value: 2 },
+  { key: 3, text: <Menu.Item as={Link} to="/company/manage-profile">
+  Change Password
+</Menu.Item>, value: 3 },
+
+{ key: 4, text: <Menu.Item as={Link} to="/company/manage-profile">
+Add Opportunity
+</Menu.Item>, value: 4 },
+
+
+]
 
 
 class CompanyProfile extends React.Component {
@@ -19,6 +35,8 @@ class CompanyProfile extends React.Component {
     super(props);
 
     this.state = {
+      companyName: "",
+      description: "",
       opportunityTitle: "",
       contactName: "",
       location: "",
@@ -32,14 +50,13 @@ class CompanyProfile extends React.Component {
 componentDidMount() {
   const companyId= 1 // will get company id from company login
    getCompanyProfile(companyId) .then(data =>{
-    console.log("this is data", data)
+    console.log("company_name", data)
+    // console.log("this is data", data)
       this.setState({
+        companyName: data.name,
+        city: data.city,
+        description: data.discription
         
-        opportunityTitle: data.opportunityTitle,
-        contactName: data.name,
-        location: data.location,
-        industry: data.industry,
-        expiryDate: data.expiringDate
       })}
     );
 }
@@ -61,10 +78,10 @@ componentDidMount() {
       <div class="ui segment">
         <Container>
           <Container text style={{ marginTop: "5em" }} border={{}}>
+          <Dropdown clearable text="Your Profile" options={options} selection />
           <Divider horizontal>
-          <h2>Company Logo</h2>
-           <h1>Company Name</h1>
-           <h2>Location</h2>
+              <Header as="h1">{this.state.companyName}</Header>
+              <Header as="h2">{this.state.city}</Header>
            </Divider>
            
            <Divider horizontal>
@@ -73,89 +90,16 @@ componentDidMount() {
              </a></segment>
 
            </Divider>
-     <div>
-           <h4>About Company</h4>
-           <p> Global leader in financial services, offering solutions
-               to the world's most important corporations, governments and 
-               institutions in more than 100 countries. 
-               We also lead volunteer service activities for employees in 
-               local communities by utilizing our many resources, 
-               including those that stem from access to capital, 
-               economies of scale, global reach and expertise.
-           </p>
-     </div>
-     <Divider horizontal>
-              <Header as="h4">Profile</Header>
-            </Divider>
-            <Grid>
-              <Grid.Column width={3}>
-                <h4>Opportunity Title:</h4>
-              </Grid.Column>
-
-              <Grid.Column width={9}>
-                <Segment secondary>
-                  <p>{this.state.opportunityTitle}</p>
-                </Segment>
-              </Grid.Column>
-            </Grid>
-
-            <Divider />
-            <Grid>
-              <Grid.Column width={3}>
-                <h4>Contact Name:</h4>
-              </Grid.Column>
-
-              <Grid.Column width={9}>
-                <Segment secondary>
-                  <p>{this.state.contactName}</p>
-                </Segment>
-              </Grid.Column>
-            </Grid>
-            <Divider />
-
-            <Divider />
-            <Grid>
-              <Grid.Column width={3}>
-                <h4>location:</h4>
-              </Grid.Column>
-
-              <Grid.Column width={9}>
-                <Segment secondary>
-                  <p>{this.state.location}</p>
-                </Segment>
-              </Grid.Column>
-            </Grid>
-            <Divider />
-            <Grid>
-              <Grid.Column width={3}>
-                <h4>Industry:</h4>
-              </Grid.Column>
-
-              <Grid.Column width={9}>
-                <Segment secondary> {this.state.industry}</Segment>
-              </Grid.Column>
-            </Grid>
-            <Divider />
-            <Grid>
-              <Grid.Column width={3}>
-                <h4>Expiring Date:</h4>
-              </Grid.Column>
-
-              <Grid.Column width={9}>
-                <Segment secondary>{this.state.expiringDate}</Segment>
-              </Grid.Column>
-              <p>
-                 Excepteur sint occaecat cupidatat non proident,
-                 sunt in culpa qui officia deserunt mollit anim.
-              </p>
-            </Grid>
+     
+           <Header as="h3">{this.state.description}</Header>
+              
           </Container>
           <Container style={{ align: "right", margin: "50px" }}>
             <Button onClick={this.handleClickEdit} primary>
-              Edit Profile
+              Edit 
             </Button>
             <Button onClick={this.handleClickDelete} secondary>
-              Delete Profile
+              Delete 
             </Button>
           </Container>
         </Container>
