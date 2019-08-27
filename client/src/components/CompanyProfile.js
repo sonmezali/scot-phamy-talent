bdce225b03191c787bd277b001cf0c76357d57a0
Phil Ezemonye <philezemonye@gmail.com>
2019-08-27 16:21:20 +0100

    CompanyProfile on component to display Edit/Delete profile and Manage Opportunities button.

--- client/src/components/CompanyProfile.js
+++ client/src/components/CompanyProfile.js
@@ -7,28 +7,48 @@ import {
   Header,
   Divider,
   Menu,
-  Dropdown,
- 
+  Dropdown
 } from "semantic-ui-react";
 import { getCompanyProfile } from "../api/companyProfile";
 const options = [
-  { key: 1, text: <Menu.Item as={Link} to="/company/manage-profile">
-  Edit Profile
-</Menu.Item> , value: 1 },
-  { key: 2, text: <Menu.Item as={Link} to="/company/manage-profile">
-Delete Profile
-</Menu.Item>, value: 2 },
-  { key: 3, text: <Menu.Item as={Link} to="/company/manage-profile">
-  Change Password
-</Menu.Item>, value: 3 },
+  {
+    key: 1,
+    text: (
+      <Menu.Item as={Link} to="/company/manage-profile">
+        Edit Profile
+      </Menu.Item>
+    ),
+    value: 1
+  },
+  {
+    key: 2,
+    text: (
+      <Menu.Item as={Link} to="/company/manage-profile">
+        Delete Profile
+      </Menu.Item>
+    ),
+    value: 2
+  },
+  {
+    key: 3,
+    text: (
+      <Menu.Item as={Link} to="/company/manage-profile">
+        Change Password
+      </Menu.Item>
+    ),
+    value: 3
+  },
 
-{ key: 4, text: <Menu.Item as={Link} to="/company/manage-profile">
-Add Opportunity
-</Menu.Item>, value: 4 },
-
-
-]
-
+  {
+    key: 4,
+    text: (
+      <Menu.Item as={Link} to="/company/manage-profile">
+        Add Opportunity
+      </Menu.Item>
+    ),
+    value: 4
+  }
+];
 
 class CompanyProfile extends React.Component {
   constructor(props) {
@@ -36,74 +56,73 @@ class CompanyProfile extends React.Component {
 
     this.state = {
       companyName: "",
-      description: "",
+      aboutCompany: "",
       opportunityTitle: "",
       contactName: "",
-      location: "",
-      industry: [],
-      email: "",
-      expiringDate: ""
+      opportunityDescription: "",
+      industry: "",
+      contact: "",
+      date: ""
     };
   }
 
-
-componentDidMount() {
-  const companyId= 1 // will get company id from company login
-   getCompanyProfile(companyId) .then(data =>{
-    console.log("company_name", data)
-    // console.log("this is data", data)
+  componentDidMount() {
+    const companyId = 1; // will get company id from company login
+    getCompanyProfile(companyId).then(data => {
+      console.log("company_name", data);
+      // console.log("this is data", data)
       this.setState({
-        companyName: data.name,
-        city: data.city,
-        description: data.discription
-        
-      })}
-    );
-}
+        companyName: data.company_name,
+        aboutCompany: data.company_description,
+        industry: data.industry,
+        opportunityTitle: data.opportunity_title,
+        contactName: data.contact_person,
+        date: data.date,
+        opportunityDescription: data.opportunity_description,
+        contact: data.email
+      });
+    });
+  }
 
   handleClickEdit = () => {
-
-    console.log("something")
-    return "/company/managa-profile"
+    console.log("something");
+    return "/company/managa-profile";
   };
 
   handleClickDelete = () => {
-
-    return "...."
+    return "....";
   };
 
   render() {
+    console.log(this.state);
     return (
-      
-      <div class="ui segment">
-        <Container>
-          <Container text style={{ marginTop: "5em" }} border={{}}>
-          <Dropdown clearable text="Your Profile" options={options} selection />
-          <Divider horizontal>
-              <Header as="h1">{this.state.companyName}</Header>
-              <Header as="h2">{this.state.city}</Header>
-           </Divider>
-           
-           <Divider horizontal>
-          <segment><a href={`mailto: ${this.state.email}`}>
-               <Button primary>Contact</Button>
-             </a></segment>
+      <Container>
+        <Dropdown clearable text="Your Profile" options={options} selection />
 
-           </Divider>
-     
-           <Header as="h3">{this.state.description}</Header>
-              
-          </Container>
-          <Container style={{ align: "right", margin: "50px" }}>
-            <Button onClick={this.handleClickEdit} primary>
-              Edit 
-            </Button>
-            <Button onClick={this.handleClickDelete} secondary>
-              Delete 
-            </Button>
-          </Container>
-        </Container>
-      </div>
+        <Header as="h1">{this.state.companyName}</Header>
+        <Divider></Divider>
+
+        <a href={`mailto: ${this.state.contact}`}>
+          <Button primary>Contact</Button>
+        </a>
+        <Header as="h2">{this.state.contact}</Header>
+
+        <Divider></Divider>
+        <h5>About Company</h5>
+        <Header as="h3">{this.state.aboutCompany}</Header>
+
+        <Header as="h4">{this.state.opportunityTitle}</Header>
+        <Header as="h5">{this.state.contactName}</Header>
+        <Header as="h6">{this.state.date}</Header>
+        <Header as="h7">{this.state.opportunityDescription}</Header>
+        <Divider></Divider>
+        <Button onClick={this.handleClickEdit} primary>
+          Edit
+        </Button>
+        <Button onClick={this.handleClickDelete} secondary>
+          Delete
+        </Button>
+      </Container>
     );
   }
 }
--- server/services/database/companyProfile.js
+++ server/services/database/companyProfile.js
@@ -5,7 +5,22 @@ const pool = new Pool(config);
 const getCompanyProfile = (id) => {
 	return new Promise((resolve, reject) => {
 		pool.query(
-			"SELECT * FROM company_profile where company_id = $1",
+			`SELECT
+  users.email,
+  company_profile.name AS company_Name,
+  company_profile.description AS company_Description,
+  company_profile.industry,
+  opportunities.name AS opportunity_title,
+  opportunities.contact_person,
+  opportunities.date,
+  opportunities.description AS opportunity_description
+FROM
+  company_profile
+  INNER JOIN opportunities ON opportunities.company_id = company_profile.company_id
+  INNER JOIN users ON users.user_id=company_profile.user_id
+WHERE
+  company_profile.company_id = $1`,
+
 			[id],
 			(error, result) => {
 				if (error) {
