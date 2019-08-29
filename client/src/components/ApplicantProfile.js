2e90a5f46d0688f2443f83560f47ce1b4b9d2364
youssef <youssef.sadek910@gmail.com>
2019-08-29 18:39:11 +0100

    Solve Conflicts 1- Deleted the Navbar component and it's content now is at the index . 2- have the name of the oppened page at the top header

--- client/src/components/ApplicantProfile.js
+++ client/src/components/ApplicantProfile.js
@@ -73,42 +73,40 @@ class ApplicantProfile extends React.Component {
 
   render() {
     return (
-      <div class="ui segment">
-        <Container>
-          <Container text style={{ marginTop: "5em" }} border={{}}>
-            <Divider horizontal>
-              <Menu compact>
-                <Dropdown text="Profile" options={options} simple item />
-              </Menu>
-            </Divider>
-            <Segment secondary>
-              <Header as="h1">{this.state.applicantName}</Header>
-              <Header as="h3">{this.state.city}</Header>
-            </Segment>
-            <Segment>
-              <a href={`mailto: ${this.state.email}`}>
-                <Button primary>Contact</Button>
-              </a>
-              <br />
-              <br />
-              {/* </Segment>
+      <Container>
+        <Container text style={{ marginTop: "5em" }} border={{}}>
+          <Divider horizontal>
+            <Menu compact>
+              <Dropdown text="Profile" options={options} simple item />
+            </Menu>
+          </Divider>
+          <Segment secondary>
+            <Header as="h1">{this.state.applicantName}</Header>
+            <Header as="h3">{this.state.city}</Header>
+          </Segment>
+          <Segment>
+            <a href={`mailto: ${this.state.email}`}>
+              <Button primary>Contact</Button>
+            </a>
+            <br />
+            <br />
+            {/* </Segment>
             <Segment> */}
-              <p>
-                Hi! My name is {this.state.applicantName}. I am a creative geek
-                from {this.state.city}. I enjoy creating eye candy solutions for
-                web and mobile apps. Contact me at {this.state.email}
-              </p>
-            </Segment>
-            <Segment>
-              <Header as="h2">Skills</Header>
-              <Divider />
-              {this.state.skills.map(skill => (
-                <Button basic>{skill}</Button>
-              ))}
-            </Segment>
-          </Container>
+            <p>
+              Hi! My name is {this.state.applicantName}. I am a creative geek
+              from {this.state.city}. I enjoy creating eye candy solutions for
+              web and mobile apps. Contact me at {this.state.email}
+            </p>
+          </Segment>
+          <Segment>
+            <Header as="h2">Skills</Header>
+            <Divider />
+            {this.state.skills.map(skill => (
+              <Button basic>{skill}</Button>
+            ))}
+          </Segment>
         </Container>
-      </div>
+      </Container>
     );
   }
 }
