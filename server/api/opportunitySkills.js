0cb3cb98e3d8cd8f36531b78e999fb93098c799b
youssef <youssef.sadek910@gmail.com>
2019-09-09 18:56:41 +0100

    Remove un used code

--- server/api/opportunitySkills.js
+++ server/api/opportunitySkills.js
@@ -2,25 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  newOpportunitySkills,
   getSkillsForOpportunitiesList
 } = require("../services/database/opportunitySkills");
-
-/**
- * The route here will be: /addSkillsToOpportunity/ (remember the prefix users is defined in api/index.js)
- */
-
-router.post("/", (req, res) => {
-  const skills = req.body.skills;
-  const opportunityId = req.body.opportunityId;
-  newOpportunitySkills(skills, opportunityId)
-    .then(data => {
-      res.send({ success: true });
-    })
-    .catch(err => {
-      res.sendStatus(500).send({ success: false });
-    });
-});
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
