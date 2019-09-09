9038076079f2cf1ad2e4a7fb11b2f44c45b78375
youssef <youssef.sadek910@gmail.com>
2019-09-09 18:50:37 +0100

    Get the skills data to applicant profile by id

--- client/src/api/applicantProfile.js
+++ client/src/api/applicantProfile.js
@@ -5,9 +5,15 @@ export const getApplicantProfileByUserId = id => {
 };
 
 export const createNewApplicantUserAndProfile = applicantEntry => {
+  console.log("applicantEntriesApi=====>", applicantEntry);
   return fetch("/api/applicantProfile/", {
     method: "POST",
     body: JSON.stringify(applicantEntry),
     headers: { "Content-Type": "application/json" }
   }).then(res => res.json());
+};
+
+// Get the skills from data base to applicant profile using id
+export const getSkillsList = id => {
+  return fetch(`/api/applicantSkills/${id}`).then(res => res.json());
 };
