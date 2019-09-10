5b85fdbd57489f5c350170f8f30fcb9db53ec8f4
Meriem <mmlys89@gmail.com>
2019-09-10 17:35:03 +0100

    Add new field logo_url to the company profile table

--- server/services/database/companyProfile.js
+++ server/services/database/companyProfile.js
@@ -2,14 +2,15 @@ const { Pool } = require("pg");
 const config = require("../../config");
 const pool = new Pool(config);
 
-const getCompanyProfile = (id) => {
-	return new Promise((resolve, reject) => {
-		pool.query(
-			`SELECT
+const getCompanyProfile = id => {
+  return new Promise((resolve, reject) => {
+    pool.query(
+      `SELECT
   users.email,
   company_profile.name AS company_Name,
   company_profile.description AS company_Description,
   company_profile.industry,
+  company_profile.logo_url,
   opportunities.name AS opportunity_title,
   opportunities.contact_person,
   opportunities.date,
@@ -19,16 +20,16 @@ const getCompanyProfile = (id) => {
   INNER JOIN users ON users.user_id=company_profile.user_id WHERE
   company_profile.company_id = $1`,
 
-			[id],
-			(error, result) => {
-				if (error) {
-					console.error(error);
-					return reject(error);
-				}
-				resolve(result.rows[0]);
-			}
-		);
-	});
+      [id],
+      (error, result) => {
+        if (error) {
+          console.error(error);
+          return reject(error);
+        }
+        resolve(result.rows[0]);
+      }
+    );
+  });
 };
 
 module.exports = { getCompanyProfile };
