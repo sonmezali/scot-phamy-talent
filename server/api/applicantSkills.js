fb1dafdd4e9cd462ec8f441b84894622b605453c
youssef <youssef.sadek910@gmail.com>
2019-09-24 22:26:05 +0100

    Get the skills for applicantProfile to display it

--- server/api/applicantSkills.js
+++ server/api/applicantSkills.js
@@ -2,10 +2,22 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  getSkillsForApplicantProfile
+  getSkillsForApplicantProfile,
+  getSkillsForEditApplicantProfile
 } = require("../services/database/applicantSkills");
 
-router.get("/:id/skills", (req, res) => {
+router.get("/getSkillsId/:id", (req, res) => {
+  const { id } = req.params;
+  getSkillsForEditApplicantProfile(id)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500);
+    });
+});
+router.get("/:id", (req, res) => {
   const { id } = req.params;
   getSkillsForApplicantProfile(id)
     .then(data => {
