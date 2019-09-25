80fdf555789a82cfe956ef381d11920a6f36a310
youssef <youssef.sadek910@gmail.com>
2019-09-25 23:10:06 +0100

    Add approve functions add approve functiins and display the waiting for approval applicant cards

--- client/src/components/AdminProfiles.js
+++ client/src/components/AdminProfiles.js
@@ -1,22 +1,32 @@
 import React, { Component } from "react";
 import { getAllApplicants } from "../api/applicants";
-import { Grid } from "semantic-ui-react";
+import { Grid, Modal, Button } from "semantic-ui-react";
 import ApplicantsCard from "./ApplicantsCard";
-import { Link } from "react-router-dom";
+import { approveApplicantProfile } from "../api/adminProfile";
+
 class AdminProfiles extends Component {
-  state = { applicantsList: [] };
+  state = { applicantsList: [], approveMessage: false, openMessage: false };
   getAllPendingApplicants = () => {
     getAllApplicants().then(res => {
-      console.log(res);
       this.setState({ applicantsList: res });
     });
   };
   componentDidMount() {
     this.getAllPendingApplicants();
   }
+  handleApprove = id => {
+    approveApplicantProfile(id).then(
+      res =>
+        res.success &&
+        this.setState({ approveMessage: res.success, openMessage: true })
+    );
+  };
+  handleCloseFail = () => this.setState({ openMessage: false });
+  handleCloseSuccess = () => {
+    this.setState({ openMessage: false });
+    window.location.reload();
+  };
   render() {
-    console.log(this.state.applicantsList);
-
     return (
       <Grid stackable>
         <Grid.Row columns={3} stretched>
@@ -25,17 +35,58 @@ class AdminProfiles extends Component {
               applicant =>
                 applicant &&
                 applicant.application_status === "pending" && (
-                  <Grid.Column
-                    key={applicant.applicant_id}
-                    as={Link}
-                    to={`/applicant-profile/${applicant.applicant_id}`}
-                  >
-                    <ApplicantsCard {...applicant} adminOptions />
+                  <Grid.Column key={applicant.applicant_id}>
+                    <ApplicantsCard
+                      {...applicant}
+                      adminOptions
+                      handleApprove={() =>
+                        this.handleApprove(applicant.applicant_id)
+                      }
+                    />
                     <br></br>
                   </Grid.Column>
                 )
             )}
         </Grid.Row>
+        {this.state.approveMessage === true && (
+          <Modal
+            open={this.state.openMessage}
+            onClose={this.handleCloseSuccess}
+            closeIcon
+            basic
+            size="small"
+          >
+            <Modal.Header> Approved successfully</Modal.Header>
+            <Modal.Actions>
+              <Button
+                positive
+                icon="checkmark"
+                labelPosition="right"
+                content="OK"
+                onClick={this.handleCloseSuccess}
+              />
+            </Modal.Actions>
+          </Modal>
+        )}
+        {this.state.approveMessage === false && (
+          <Modal
+            basic
+            open={this.state.openMessage}
+            onClose={this.handleCloseFail}
+            closeIcon
+          >
+            <Modal.Header> Something went Wrong</Modal.Header>
+            <Modal.Actions>
+              <Button
+                negative
+                icon="checkmark"
+                labelPosition="right"
+                content="OK"
+                onClick={this.handleCloseFail}
+              />
+            </Modal.Actions>
+          </Modal>
+        )}
       </Grid>
     );
   }
--- client/src/components/ApplicantsCard.js
+++ client/src/components/ApplicantsCard.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { Card, Icon, Divider, Button } from "semantic-ui-react";
 
-export default ({ applicant_name, city, skills, adminOptions }) => (
+export default ({
+  applicant_name,
+  city,
+  skills,
+  adminOptions,
+  handleApprove
+}) => (
   <Card centered raised color="blue">
     <Card.Content>
       <Card.Header textAlign="left">
@@ -14,21 +20,23 @@ export default ({ applicant_name, city, skills, adminOptions }) => (
           City: {city}
         </Card.Content>
       )}
-      <Card.Content>
-        <Icon name="check" size="large" color="blue" />
-        Skills:
-        {skills &&
-          skills.map((skill, index) => (
-            <Card.Content key={index} textAlign="left">
-              {skill}
-            </Card.Content>
-          ))}
-      </Card.Content>
+      {skills && (
+        <Card.Content>
+          <Icon name="check" size="large" color="blue" />
+          Skills:
+          {skills &&
+            skills.map((skill, index) => (
+              <Card.Content key={index} textAlign="left">
+                {skill}
+              </Card.Content>
+            ))}
+        </Card.Content>
+      )}
       {adminOptions && (
         <Card.Content extra>
           <Divider></Divider>
           <div className="ui two buttons">
-            <Button basic color="green">
+            <Button basic color="green" onClick={handleApprove}>
               Approve
             </Button>
             <Button basic color="red">
