6bd61b6ee4a704e28452113737a59d864668a694
youssef <youssef.sadek910@gmail.com>
2019-09-27 23:52:18 +0100

    Add more cities and skills to database

---
+++ server/db/cities.sql
@@ -0,0 +1,204 @@
+INSERT INTO
+  cities (city)
+VALUES
+  ('Edinburgh');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Aberdeen');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Dundee');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Glasgow');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Hamilton');
+INSERT INTO
+  cities (city)
+VALUES
+  ('East Kilbride');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Ayr');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Dumbarton');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Bellshill');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Barrhead');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Motherwell');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Paisley');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Dunfermline');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Livingston');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Cumbernauld');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Kirkcaldy');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Perth');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Inverness');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Kilmarnock');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Coatbridge');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Greenock');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Glenrothes');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Stirling');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Airdrie');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Falkirk');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Irvine');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Dumfries');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Rutherglen');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Wishaw');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Cambuslang');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Bearsden');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Newton Mearns');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Clydebank');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Elgin');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Arbroath');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Bishopbriggs');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Bathgate');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Renfrew');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Kirkintilloch');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Musselburgh');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Peterhead');
+INSERT INTO
+  cities (city)
+VALUES
+  ('St Andrews');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Bonnyrigg');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Blantyre');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Grangemouth');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Kilwinning');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Penicuik');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Johnstone');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Viewpark');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Erskine');
+INSERT INTO
+  cities (city)
+VALUES
+  ('Broxburn');
--- server/db/populate-db.sql
+++ server/db/populate-db.sql
@@ -1,56 +1,6 @@
 -- Users seed data
--- Note: Do NOT add passwords you use in real life. Passwords are now saved as clear text. This is very bad from security point of view. We will have a task to encrypt the passwords
-
-INSERT INTO cities
-    (city)
-VALUES
-    ('Aberdeen');
-
-INSERT INTO cities
-    (city)
-VALUES
-    ('Dundee');
-
-INSERT INTO cities
-    (city)
-VALUES
-    ('Glasgow');
-
-INSERT INTO cities
-    (city)
-VALUES
-    ('Hamilton');
-
-INSERT INTO cities
-    (city)
-VALUES
-    ('East Kilbride');
-
-INSERT INTO cities
-    (city)
-VALUES
-    ('Ayr');
-
-INSERT INTO cities
-    (city)
-VALUES
-    ('Dumbarton');
-
-INSERT INTO cities
-    (city)
-VALUES
-    ('Bellshill');
-
-INSERT INTO cities
-    (city)
-VALUES
-    ('Barrhead');
-
-INSERT INTO cities
-    (city)
-VALUES
-    ('Motherwell');
-
+-- Note: Do NOT add passwords you use in real life. Passwords are now saved as clear text. 
+-- This is very bad from security point of view. We will have a task to encrypt the passwords
 INSERT INTO users
     (ROLE, email, PASSWORD)
 VALUES
@@ -131,25 +81,6 @@ INSERT INTO opportunities
 VALUES
     ('Volunteer with cyf', 'Five months in voluntary work', 'Phil', '0444445555', 'yan@cyf.com', 5, '2020-01-15', 'Volunteer', 1);
 
-INSERT INTO skills
-    (name)
-VALUES
-    ('JavaScript');
-
-INSERT INTO skills
-    (name)
-VALUES
-    ('Graphic Design');
-
-INSERT INTO skills
-    (name)
-VALUES
-    ('Information Architectur');
-
-INSERT INTO skills
-    (name)
-VALUES
-    ('Mechanical Engineering');
 INSERT INTO opportunity_skills
     (skill_id, opportunity_id)
 VALUES
