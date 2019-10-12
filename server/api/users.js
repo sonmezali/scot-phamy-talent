23179c05ce2f5dc182a4debf60e19851639fcb84
youssef <youssef.sadek910@gmail.com>
2019-10-13 00:22:11 +0100

    Create ChangePassword Api

--- server/api/users.js
+++ server/api/users.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const usersDb = require("../services/database/users");
+const { getAllUsers, editPassword } = require("../services/database/users");
 
 /**
  * The route here will be: /users/ (remember the prefix users is defined in api/index.js)
  */
 router.get("/", (req, res) => {
-	usersDb
-		.getAllUsers()
-		.then((data) => {
-			res.send(data);
-		})
-		.catch((err) => {
-			console.error(err);
-			res.status(500);
-		});
+  getAllUsers()
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500);
+    });
 });
 
+router.put("/change-password", (req, res) => {
+  const { userId, newPassword } = req.body;
+  editPassword({ userId, newPassword })
+    .then(data => res.send(data))
+    .catch(err => {
+      res.status(500);
+    });
+});
 module.exports = router;
