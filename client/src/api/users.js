7e7ea55da022cd79bd4cbf34d231b34f31d1e915
youssef <youssef.sadek910@gmail.com>
2019-10-15 04:04:26 +0100

    Add delete user function

--- client/src/api/users.js
+++ client/src/api/users.js
@@ -12,3 +12,9 @@ export const changePassword = (userId, newPassword) => {
 export const getUserById = id => {
   return fetch(`/api/users/${id}`).then(res => res.json());
 };
+
+export const deleteUser = id => {
+  return fetch(`/api/users/delete-user/${id}`, { method: "DELETE" }).then(res =>
+    res.json()
+  );
+};
